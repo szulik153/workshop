a8f1286e58cb91172bd5c2b13fbd5d4b75a2e089
Martyn Szulczewski <martyn.szulczewski@capgemini.com>
2022-12-15 09:13:11 +0100

    feat: add ngrx

--- apps/communication/src/main.ts
+++ apps/communication/src/main.ts
@@ -4,11 +4,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(HttpClientModule), provideAnimations()],
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    provideAnimations(),
+    provideStore(),
+    provideStoreDevtools(),
+  ],
 }).catch((err) => console.error(err));
